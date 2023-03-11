428cd21b66974cd68543a547e452e01281970504
sssungjooon <dellojoon7@gmail.com>
2023-03-11 15:56:56 +0900

    23.03.11 study

---
+++ why-ts/sample.ts
@@ -0,0 +1,12 @@
+function sum(a: number, b: number) {
+  return a + b;
+}
+sum(10, 20);
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+var result = add(10, 20);
+result.toLocaleString();
+
+// add(10, '20');
